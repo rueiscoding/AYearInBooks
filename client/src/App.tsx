1a442e0f7bb320353ae5aab23252bfaa2525f807
rueiscoding <snixorstix@gmail.com>
2025-08-23 20:03:25 -0400

    added styles in tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,13 @@
 import { useState, useRef, useEffect } from 'react';
+import './index.css';
+
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [output, setOutput] = useState<any | null>(null);
   const [wrapId, setWrapId] = useState<string | null>(null);
 
-
-  
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setFile(file);
-    }
-
-  };
 
   const handleUpload = async() => {
     // console.log("inhere");
@@ -51,8 +45,8 @@ function App() {
     
     const fetchData = async () =>{
       const res = await fetch(`http://localhost:3001/api/v1/csv/data/${wrapId}`);
-      const text = await res.text();
-      console.log("CSV contents", text);
+      const text = await res.json();
+      setOutput(text);
     }
 
     fetchData();
@@ -62,21 +56,29 @@ function App() {
   
   return (
 
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center pt-5">
 
-        <h1 className='text-center font-bold text-3xl pt-8 pb-8'>Goodreads Wrapped</h1>
-        <div className='border-3 rounded-md border-black size-40 hover:cursor-pointer' onClick={triggerFileInput}>
+        <h1 className='text-center font-bold text-3xl pt-8 pb-8 font-header'>Goodreads Wrapped</h1>
+
+        {output === null &&
+        <div className='border-3 rounded-md border-toast size-40 hover:cursor-pointer' onClick={triggerFileInput}>
+
+          <input type="file" ref={fileInputRef} accept=".csv, text/csv" style={{ display: 'none' }}
+          onChange={(e) => {
+          if (e.target.files && e.target.files.length > 0) {
+            setFile(e.target.files[0]);}}}/>
 
           {!fileInputRef.current&& (
-            <p className='text-center pt-15'>click to upload csv</p>
+            <p className='text-center pt-15 font-body'>click to upload csv</p>
           )}
         
+        </div>}
 
-          <input type="file" accept=".csv" ref={fileInputRef} onChange={handleFileChange} className="hidden"/>
-          {file && (
-            <p className="text-center pt-15">cooking your stats!</p>
-          )}
-        </div>
+        {output !== null &&
+          <div>
+            <p>{JSON.stringify(output, null, 2)}</p>
+          </div>
+        }
 
       </div> 
   )
